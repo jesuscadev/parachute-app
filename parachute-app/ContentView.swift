10d29b38a6f44f7951d1f078564422606ceea088
jesuscadev <jesuscadev@icloud.com>
2021-05-20 23:40:25 -0500

    Initial commit

---
+++ parachute-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  parachute-app
+//
+//  Created by Jesús Casillas on 20/05/21.
+//  Copyright © 2021 Jesús Casillas. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
